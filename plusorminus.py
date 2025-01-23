b6504552876bab92e8b73d5c60c4590f6377ffaa
hilelooo <61366309+hilelooo@users.noreply.github.com>
2025-01-23 10:29:47 +0000

    Update plusorminus.py

--- plusorminus.py
+++ plusorminus.py
@@ -1,4 +1,9 @@
 import random
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 
 def game(max, lives):
@@ -13,6 +18,7 @@ def game(max, lives):
       print("ouais c'est ca")
       break
     lives -= 1
+    clear()
     print(f"Il vous reste {lives} vies")
   if lives == 0:
     print("t nul")
