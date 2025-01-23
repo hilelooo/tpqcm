33d713b051de6937d47338daf96dae41e806b9e7
hilelooo <61366309+hilelooo@users.noreply.github.com>
2025-01-23 10:07:39 +0000

    Create plusorminus.py

---
+++ plusorminus.py
@@ -0,0 +1,16 @@
+import random
+
+
+def game(max, lives = 10):
+  number = random.randint(1,max)
+  while lives > 0:
+    guess = int(input("Choix"))
+    if guess > number:
+      print("Moins")
+    else if guess < number:
+      print("Plus")
+    else if guess == number:
+      print("ouais c'est ca")
+      break
+  if lives == 0:
+    print("t nul")
